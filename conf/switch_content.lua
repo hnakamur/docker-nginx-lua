153aaecd1fad192eb29685950134609f5187bbaf
Hiroaki Nakamura <hnakamur@gmail.com>
2014-03-26 12:00:00 +0900

    Modify for a new data structure in redis

--- conf/switch_content.lua
+++ conf/switch_content.lua
@@ -1,40 +1,50 @@
+function contained(comma_list_str, str)
+  for v in string.gmatch(comma_list_str, "[^,]+") do
+    if v == str then
+      return true
+    end
+  end
+  return false
+end
+
 local granted = false
 local session_id = ngx.var.cookie_inter_app_session
 if (session_id ~= nil) then
-    local redis = require "resty.redis"
-    local client = redis:new()
-    client:connect(os.getenv("REDIS_PORT_6379_TCP_ADDR"), tonumber(os.getenv("REDIS_PORT_6379_TCP_PORT")))
-    local tasks, err = client:get(session_id)
-    if not tasks then
-        ngx.say("failed to get from redis: session_id=", session_id, ", err=", err)
-        ngx.exit(500)
-        return
-    end
+  local redis = require "resty.redis"
+  local client = redis:new()
+  client:connect(os.getenv("REDIS_PORT_6379_TCP_ADDR"), tonumber(os.getenv("REDIS_PORT_6379_TCP_PORT")))
+  local tasks, err = client:get(session_id)
+  if not tasks then
+    ngx.say("failed to get from redis: session_id=", session_id, ", err=", err)
+    ngx.exit(500)
+    return
+  end
 
-    if tasks ~= ngx.null then
-        local task_from_uri = string.match(ngx.var.uri, '^/([^/]*)')
-        ngx.header["X-Debug-tasks"] = { 'tasks=' .. tasks }
-        for task in string.gmatch(tasks, "[^,]+") do
-            if task_from_uri == task then
-                granted = true
-                ngx.header["X-Debug-granted"] = { string.format('granted=%s', granted) }
-                break
-            end
-        end
+  if tasks ~= ngx.null then
+    local value = client:hget('file', ngx.var.uri)
+    if value ~= ngx.null then
+      local task_id, op = string.match(value, '^([^:]*):(.*)$')
+      ngx.header["X-Debug-task-id"] = { 'task_id=' .. task_id }
+      ngx.header["X-Debug-op"] = { 'op=' .. op }
+      if contained(tasks, task_id) then
+        granted = true
+      end
     end
+  end
 
-    -- put it into the connection pool of size 100,
-    -- with 10 seconds max idle time
-    local ok, err = client:set_keepalive(10000, 100)
-    if not ok then
-        ngx.say("failed to set keepalive: ", err)
-        ngx.exit(500)
-        return
-    end
+  -- put it into the connection pool of size 100,
+  -- with 10 seconds max idle time
+  local ok, err = client:set_keepalive(10000, 100)
+  if not ok then
+    ngx.say("failed to set keepalive: ", err)
+    ngx.exit(500)
+    return
+  end
 end
 
+ngx.header["X-Debug-granted"] = { string.format('granted=%s', granted) }
 if granted then
-    ngx.exec("@private")
+  ngx.exec("@private")
 else
-    ngx.exec("@public")
+  ngx.exec("@public")
 end
